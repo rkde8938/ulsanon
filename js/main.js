56e58c2c05fb68675c78a2f118a2c5d90fd664cb
다은 <takem@teikeuem-ui-iMac.local>
2022-04-15 14:31:29 +0900

    회견문 및 이미지 추가

--- js/main.js
+++ js/main.js
@@ -22,3 +22,18 @@ let cubeHeight = function () {
 
 cubeHeight();
 $(window).resize(cubeHeight);
+
+
+// 카피라이트 현재 년도 표시
+const thisYear = document.querySelector('.this-year');
+thisYear.textContent = new Date().getFullYear(); // 현재의 년도 (2022)
+
+//드롭다운 메뉴 작동
+$('.menu__button').click(function () {
+  let dropdownMenu = $('#dropdown_menu ul');
+  if (dropdownMenu.height() > 0) {
+    dropdownMenu.css('height', '0');
+  } else {
+    dropdownMenu.css('height', '312px');
+  };
+});
